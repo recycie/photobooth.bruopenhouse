efbf8992dd3af585a7d3bb795d530b22e82e0965
recycie <kasidetpete@gmail.com>
2024-08-31 05:21:14 +0700

    Fixed Commants

--- config.php
+++ config.php
@@ -7,5 +7,5 @@ define('CONFIGFILE', 'config.json');
 define('CONFIGFILE_ADMIN', '../config.json');
 
 define('SHOTCOUNTDOWN', 3); // เคาท์ดาวน์ถ่ายรูป
-define('ENPOINT_URL_DOWNLOAD', 'https://pants-announced-briefly-correct.trycloudflare.com'); // ลิงก์ Publice สำหรับใช้โหลดรูป ตัวอย่าง. https://localhost (ลิงก์ต้องไม่มีเครื่องหมาย / อยู่หลังสุด)
+define('ENPOINT_URL_DOWNLOAD', 'https://pants-announced-briefly-correct.trycloudflare.com'); // URL Public สำหรับใช้โหลดรูป ตัวอย่าง. https://localhost (URL ต้องไม่มีเครื่องหมาย / อยู่หลังสุด)
 ?>
